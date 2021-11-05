e2721f74e2a172f44fbff15f4b9dec586082d074
Christoffer-Nielsen-Cph <cph-cn332@cphbusiness.dk>
2021-11-05 10:31:30 +0100

    Started coding on hold

--- SP3/src/Bordfodboldturnering/Hold.java
+++ SP3/src/Bordfodboldturnering/Hold.java
@@ -1,11 +1,21 @@
 package Bordfodboldturnering;
 
+import java.util.ArrayList;
+
 public class Hold {
     private String holdNavn;
     private int antalSpillere;
+    private int point;
+    private int målScore;
+    private ArrayList<Kamp>kampHistorik=new ArrayList<>();
 
     public Hold(String holdNavn, int antalSpillere) {
         this.holdNavn = holdNavn;
         this.antalSpillere = antalSpillere;
+
+    }
+    public int beregnMålscore(){
+
+        return målScore;
     }
 }
