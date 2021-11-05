8d6302eaf99eb7f91d12f6137890516dd140b531
Christoffer-Nielsen-Cph <cph-cn332@cphbusiness.dk>
2021-11-05 11:10:21 +0100

    Hold updates

--- SP3/src/Bordfodboldturnering/Hold.java
+++ SP3/src/Bordfodboldturnering/Hold.java
@@ -1,9 +1,6 @@
 package Bordfodboldturnering;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Hold {
     private String holdNavn;
@@ -17,19 +14,6 @@ public class Hold {
         this.antalSpillere = antalSpillere;
 
     }
-
-  /*  public void læsHoldData(String data) throws FileNotFoundException {
-        File file = new File(data);
-        Scanner sc = new Scanner(file);
-        while(sc.hasNext()){
-            String line = sc.nextLine();
-            String[] lineArray = line.split(";");
-            int holdIDtmp = Integer.parseInt(lineArray[0]);
-            int antalSpilleretmp = Integer.parseInt(lineArray[2]);
-            Hold hold = new Hold(holdIDtmp,lineArray[1],antalSpilleretmp);
-            System.out.println(hold);
-        }
-    }*/
 
     @Override
     public String toString(){
