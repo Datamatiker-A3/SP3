a4034447901c385a4e3fc5e01fce91a11f29b7f5
AndersMeinicke <andersmeinicke@gmail.com>
2021-11-08 12:18:56 +0100

    Hello det er Anders, frick you.

--- SP3/src/Bordfodboldturnering/Hold.java
+++ SP3/src/Bordfodboldturnering/Hold.java
@@ -1,11 +1,9 @@
 package Bordfodboldturnering;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Hold {
+    private int Points;
     private String holdNavn;
     private int antalSpillere;
     private int holdID;
@@ -18,18 +16,13 @@ public class Hold {
 
     }
 
-  /*  public void læsHoldData(String data) throws FileNotFoundException {
-        File file = new File(data);
-        Scanner sc = new Scanner(file);
-        while(sc.hasNext()){
-            String line = sc.nextLine();
-            String[] lineArray = line.split(";");
-            int holdIDtmp = Integer.parseInt(lineArray[0]);
-            int antalSpilleretmp = Integer.parseInt(lineArray[2]);
-            Hold hold = new Hold(holdIDtmp,lineArray[1],antalSpilleretmp);
-            System.out.println(hold);
-        }
-    }*/
+    public int getPoints() {
+        return Points;
+    }
+
+    public void setPoints(int points) {
+        Points = points;
+    }
 
     @Override
     public String toString(){
--- SP3/src/Bordfodboldturnering/Turnering.java
+++ SP3/src/Bordfodboldturnering/Turnering.java
@@ -1,4 +1,12 @@
 package Bordfodboldturnering;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 public abstract class Turnering {
+    ArrayList<Hold> holdListe = new ArrayList<>();
+    abstract void læsHoldData(String s)throws FileNotFoundException;
+    abstract void fjernOverskud();
+    abstract void opsætKamp(int i);
 }
+
