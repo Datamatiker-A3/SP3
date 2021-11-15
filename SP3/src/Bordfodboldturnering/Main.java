94ab5a342f0ae26133234190b898dcb4ce9c9d48
AndersMeinicke <andersmeinicke@gmail.com>
2021-11-15 09:49:17 +0100

    Hello det er Anders, ændrede Klassediagram.puml

--- SP3/src/Bordfodboldturnering/Main.java
+++ SP3/src/Bordfodboldturnering/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
         KnockOutTurnering turnering = new KnockOutTurnering();
-        turnering.læsHoldData("SP3/resources/hold_data.txt");
+        turnering.LæsFilData("SP3/resources/hold_data.txt");
         turnering.kampCyklus();
 
     }
