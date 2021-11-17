c1a89b0405a251ff69a3748223e3881ebd94f7a2
Christoffer-Nielsen-Cph <cph-cn332@cphbusiness.dk>
2021-11-17 11:51:58 +0100

    Done with UI

--- SP3/src/Bordfodboldturnering/Main.java
+++ SP3/src/Bordfodboldturnering/Main.java
@@ -6,8 +6,6 @@ public class Main {
 
 
     public static void main(String[] args) throws FileNotFoundException {
-        KnockOutTurnering turnering = new KnockOutTurnering();
-        turnering.LæsFilData("SP3/resources/hold_data.txt");
         UI ui = new UI();
         ui.menu();
 
