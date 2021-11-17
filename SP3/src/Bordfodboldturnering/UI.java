56754dc3ecbba807482883f2af077f8700519ded
Christoffer-Nielsen-Cph <cph-cn332@cphbusiness.dk>
2021-11-17 11:44:54 +0100

    Done with UI

--- SP3/src/Bordfodboldturnering/UI.java
+++ SP3/src/Bordfodboldturnering/UI.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 public class UI {
     int input;
     boolean quit = false;
+    boolean isFilLæser = false;
+    boolean isDataBase = false;
     ArrayList<Hold> hold = new ArrayList<>();
     FilLæser filLæser = new FilLæser();
     DBConnector dbConnector = new DBConnector();
@@ -21,10 +23,12 @@ public class UI {
                 case 1:
                     hold = filLæser.læsHoldData("SP3/resources/hold_data.txt");
                     System.out.println("Holddata er nu indlæst fra tekstfilen");
+                    isFilLæser = true;
                     break;
                 case 2:
-                    dbConnector.readFieldData();
+                    hold = dbConnector.readFieldData();
                     System.out.println("Holddata er nu indlæst fra databasen");
+                    isDataBase = true;
                     break;
         }
         System.out.println("Tast 1 for at se holdnavnene");
@@ -35,13 +39,20 @@ public class UI {
         input = sc.nextInt();
         switch(input) {
             case 1:
-                for (Hold hold : filLæser.holdListe) {
-                    System.out.println(hold.getHoldNavn());
+                if(isFilLæser == true) {
+                    for (Hold hold : filLæser.holdListe) {
+                        System.out.println(hold.getHoldNavn());
+                    }
+                }
+                if(isDataBase == true){
+                    for(Hold hold : dbConnector.holdListeDatabase){
+                        System.out.println(hold.getHoldNavn());
+                    }
                 }
                 break;
             case 2:
-                turnering.holdListe = hold;
-                turnering.kampCyklus();
+                    turnering.holdListe = hold;
+                    turnering.kampCyklus();
                 break;
             case 3:
                 System.out.println("Der kan ikke oprettes en point turnering på nuværende tidspunkt");
